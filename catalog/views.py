278b878793b4d7b79ddb67de18bea174b0558242
unixlamaster <unixlamaster@mail.ru>
2018-06-27 18:54:18 +0300

    add templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("<h2>Hi !!!</h2>")
+#    return HttpResponse("<h2>Hi !!!</h2>")
+    template = loader.get_template('main.htm')
+    context = {
+        '1': 1,
+        'title_html': "Hi",
+        'body_html': "Site",
+    }
+#    return render(request, 'main.htm')
+    return HttpResponse(template.render(context, request))
