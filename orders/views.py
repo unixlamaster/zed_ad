0c72540b2e01fedf5d4ef9c89d159804bd149d76
unixlamaster <unixlamaster@mail.ru>
2018-08-13 22:00:52 +0300

    new file:   catalog/templates/orders.htm 	new file:   orders/__init__.py 	new file:   orders/__pycache__/__init__.cpython-36.pyc 	new file:   orders/__pycache__/admin.cpython-36.pyc 	new file:   orders/__pycache__/apps.cpython-36.pyc 	new file:   orders/__pycache__/models.cpython-36.pyc 	new file:   orders/__pycache__/urls.cpython-36.pyc 	new file:   orders/__pycache__/views.cpython-36.pyc 	new file:   orders/admin.py 	new file:   orders/apps.py 	new file:   orders/migrations/__init__.py 	new file:   orders/migrations/__pycache__/__init__.cpython-36.pyc 	new file:   orders/models.py 	new file:   orders/tests.py 	new file:   orders/urls.py 	new file:   orders/views.py

---
+++ orders/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.http import JsonResponse
+from catalog.models import UserSession,Price
+from django.contrib.auth.models import User
+from django.contrib.sessions.models import Session
+from django.forms.models import model_to_dict
+import logging
+
+    
+def index(request):
+    logger = logging.getLogger(__name__)
+    template = loader.get_template('orders.htm')
+    context = {
+        '1': 1,
+        'title_html': "Hi",
+        'body_html': "Site",
+        
+    }
+#    return render(request, 'main.htm')
+    logger.error("--------------")
+#    check_session(request)
+    return HttpResponse(template.render(context, request))
+
+# Create your views here.
