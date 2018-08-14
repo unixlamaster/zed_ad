137c8bf506a4e7e8484aeb9e883be58d8e3e8508
unixlamaster <unixlamaster@mail.ru>
2018-08-14 22:14:21 +0300

    preload views auth

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
+    url('/orders/login', views.login, name="order_login"),
     url(r'^$', views.index, name="order_index_url"),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,17 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from django.http import JsonResponse
 from catalog.models import UserSession,Price
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.sessions.models import Session
 from django.forms.models import model_to_dict
 import logging
 
-    
+def login(request):
+	template = loader.get_template('auth.htm')
+	context = {
+		'1': 1,
+		'title_html': "Hi",
+		'body_html': "Site",
+        'login_alert': 0,
+	}
+	return HttpResponse(template.render(context, request))    
+
 def index(request):
 	logger = logging.getLogger(__name__)
+    if not request.user.is_authenticated():
+        return redirect('/orders/login')
 	user = authenticate(username=request.POST['login'], password=request.POST['password']) if request.method == "POST" else None
 	if user is not None:
 		# A backend authenticated the credentials
@@ -28,6 +39,7 @@ def index(request):
 			'1': 1,
 			'title_html': "Hi",
 			'body_html': "Site",
+            'login_alert': 0,
 		}
 #    return render(request, 'main.htm')
 	logger.error("--------------")
